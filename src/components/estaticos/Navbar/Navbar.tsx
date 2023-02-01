637799e39f8abdb30141068786bf3114f4159dcc
guhms1998 <gustavohms1998@gmail.com>
2023-02-01 17:59:32 -0300

    0.9.7

--- src/components/estaticos/Navbar/Navbar.tsx
+++ src/components/estaticos/Navbar/Navbar.tsx
@@ -13,7 +13,7 @@ function Navbar ({acao=true}) {
       <Grid  paddingTop={1} xs={6}  >
         <Link href="/">
         
-          <img src="src\components\estaticos\Navbar\Hms.png" width={'50vh'} height={'50vh'}  className="img1"  />          
+          <img src="src\components\estaticos\Navbar\fhms.png" width={'70vh'} height={'65vh'}  className="img1"/>          
       </Link>
       </Grid>
       <Grid  xs={5}  display="flex" alignItems="center" justifyContent="center" rowSpacing={2} className='nav2'>
